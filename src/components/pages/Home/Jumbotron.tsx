a0f2297e0c4d170754ea8b48e031d8546fac728d
riandyhasan <riandyhsn@gmail.com>
2023-02-13 16:44:05 +0700

    fix: change bot name

--- src/components/pages/Home/Jumbotron.tsx
+++ src/components/pages/Home/Jumbotron.tsx
@@ -40,7 +40,7 @@ export default function Jumbotron() {
           <h1>Register/Login</h1>
           <Image src={SecurityAssets} alt={'Modal Assets'} style={{ width: '70%', height: '70%' }} />
           <br />
-          <TelegramLoginButton dataOnAuth={handleTelegramResponse} botName='lou_bookkeeper_dev_bot' />{' '}
+          <TelegramLoginButton dataOnAuth={handleTelegramResponse} botName='OdauBot' />{' '}
         </div>
       </Modal>
       <div className={styles['jumbot-left']}>
